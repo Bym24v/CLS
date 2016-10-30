3595d07d915b9c063526b18a8c850c8502e96727
Bym24v <manuel15986@hotmail.com>
2016-10-30 18:49:09 +0100

    Update

---
+++ UsbManager/UsbManager/Config.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UsbManagerDemo
+{
+    public partial class Config : Form
+    {
+        static string mPath = Directory.GetCurrentDirectory();
+
+        public Config()
+        {
+            InitializeComponent();
+
+            textBox1.Text = USBController.USB;
+
+            //USBController.ReadFileSecureKey();
+            //textBox3.Text = USBController.USBGuardado;
+            //Console.WriteLine(USBController.USB);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Guardar USB
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            USBController.SetTextSecureKey(ProcesadorID.GetProcesador() + " " + USBController.USB + " " + textBox4.Text, textBox4.Text);
+            //USBController.GetTextSecureKey("0S1eklsMbQOw+afCgNgbzFKScN1xR0rx2VkPr1N3tVljocCgiT7qDvklPZUCbsR8vElxh5+Y6s8xHzHh4gPhSw==", textBox4.Text);
+
+            USBController.ReadFileSecureKey();
+            textBox3.Text = USBController.USBGuardado;
+            
+        }
+    }
+}
---
+++ UsbManager/UsbManager/USBController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace UsbManagerDemo
+{
+    public class USBController
+    {
+        private static string tbText4;
+        private static string usbGuardado;
+        private static string usb;
+        private static string decryp;
+
+        static string mPath = Directory.GetCurrentDirectory();
+
+        public void UsbController(string _usb)
+        {
+            usb = _usb;
+        }
+
+        public static void SecureKeyFileCheck()
+        {
+            if (!File.Exists(mPath + "\\SucureKey.txt"))
+            {
+                File.Create(mPath + "\\SucureKey.txt");
+            }
+        }
+
+        public static void SetTextSecureKey(string data, string key)
+        {
+            //Console.WriteLine(ProcesadorID.GetProcesador());
+            Console.WriteLine(MD5Generator.Encrypt(data, key));
+
+            if (!File.Exists(mPath + "\\SucureKey.txt"))
+            {
+                File.Create(mPath + "\\SucureKey.txt");
+            }
+            else
+            {
+                TextWriter tw = new StreamWriter(mPath + "\\SucureKey.txt");
+                decryp = MD5Generator.Encrypt(data, key);
+                tw.WriteLine(MD5Generator.Encrypt(data, key));
+                tw.Close();
+            }
+            
+        }
+
+        public static void GetTextSecureKey(string data, string key)
+        {
+            string decrypString = MD5Generator.Decrypt(data, key);
+            Console.WriteLine(decrypString);
+            
+        }
+
+        public static void ReadFileSecureKey()
+        {
+            //TextReader tr = new StreamReader(mPath + "\\SucureKey.txt");
+            //usbGuardado = tr.ReadToEnd().ToString();
+            //Console.WriteLine(tr.ReadToEnd().ToString());
+
+            StreamReader sr = new StreamReader(mPath + "\\SucureKey.txt");
+            {
+                String line = sr.ReadToEnd();
+                usbGuardado = line;
+            }
+        }
+
+        public static string TextBox4
+        {
+            get 
+            { 
+                return tbText4; 
+            }
+            set
+            {
+                value = tbText4;
+            }
+        }
+
+        public static string USB
+        {
+            get
+            {
+                return usb;
+            }
+            set 
+            {
+                value = usb;
+            }
+        }
+
+        public static string USBGuardado
+        {
+            get
+            {
+                return usbGuardado;
+            }
+            set
+            {
+                value = usbGuardado;
+            }
+        }
+    }
+}
